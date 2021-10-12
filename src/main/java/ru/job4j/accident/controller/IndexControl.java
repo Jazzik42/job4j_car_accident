0b368dee2eb2c5c50f54dcda46a35a5618caaa6f
Jazzik42 <rmuzrrr@gmail.com>
2021-10-12 21:06:31 +0500

    1. Загрузка данных в вид [#295782 #214952]

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
+
     @GetMapping("/")
     public String index(Model model) {
+        List<String> users = new ArrayList<>();
+        users.add("Ramil Muz");
+        users.add("Ramil Muz1");
+        users.add("Ramil Muz2");
+        model.addAttribute("users", users);
         return "index";
     }
 }
